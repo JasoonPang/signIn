4b632c995e1d50a9c2f75c1ba4fe7aa563490f44
JasoonPang <38550758+JasoonPang@users.noreply.github.com>
2024-08-16 12:02:29 +0800

    Update iqiyi.js

--- function/iQIYI-DailyBonus/iqiyi.js
+++ function/iQIYI-DailyBonus/iqiyi.js
@@ -20,9 +20,9 @@ const SEND_KEY = process.env.SEND_KEY
 const UTC8 = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000;
 
 async function downFile () {
-    // const url = 'https://raw.githubusercontent.com/BlueSkyClouds/Script/master/nodejs/iQIYI-bak.js'
-    const url = 'https://github.com/BlueSkyClouds/Script/blob/master/nodejs/iQIYI-bak.js'
+    const url = 'https://raw.githubusercontent.com/BlueSkyClouds/Script/master/nodejs/iQIYI-bak.js'
     await download(url, './')
+    exec('cat ./iQIYI-bak.js')
 }
 
 async function changeFiele () {
